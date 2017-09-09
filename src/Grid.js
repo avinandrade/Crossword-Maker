efc4156563288fa3ca1e89cd19b13a664c49a470
Chris Hallacy <chris@nuna.com>
2017-09-08 23:23:27 -0700

    Renamed squares to columns

--- src/Grid.js
+++ src/Grid.js
@@ -7,19 +7,19 @@ class Grid extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      numberOfRows : 15,
-      numberOfColumns : 15
+      numberOfRows : 4,
+      numberOfColumns : 2
     }
   }
   render() {
   const rows = Array.apply(null, {length: this.state.numberOfRows}).map(Number.call, Number)
-  const squares = Array.apply(null, {length: this.state.numberOfColumns}).map(Number.call, Number)
+  const columns = Array.apply(null, {length: this.state.numberOfColumns}).map(Number.call, Number)
     return (
     <div className='grid'>
       {rows.map(function(i) {
         return(
           <div key={`row-${i}`} className='grid-row'>
-            {squares.map(function(j) {
+            {columns.map(function(j) {
               return(
                 <div key={`square-${j}`} className='grid-square'/>
               )
