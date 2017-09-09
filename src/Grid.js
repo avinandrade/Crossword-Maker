ea11f5d3c6d54a89c24053234ab3aeef7af27f49
avin <avindandrade@gmail.com>
2017-09-08 23:15:28 -0700

    made a working grid

--- src/Grid.js
+++ src/Grid.js
@@ -8,12 +8,12 @@ class Grid extends Component {
     super(props);
     this.state = {
       numberOfRows : 15,
-      numberOfColumns : 20
+      numberOfColumns : 15
     }
   }
+  render() {
   const rows = Array.apply(null, {length: this.state.numberOfRows}).map(Number.call, Number)
   const squares = Array.apply(null, {length: this.state.numberOfColumns}).map(Number.call, Number)
-  render() {
     return (
     <div className='grid'>
       {rows.map(function(i) {
